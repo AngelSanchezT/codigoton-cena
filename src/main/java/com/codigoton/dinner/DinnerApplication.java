4daa9692ea812c98339fec835cd470ddef96481a
Angel Sanchez <ajsanchez22@gmail.com>
2021-10-24 02:59:03 -0500

    Configuracion de Log de la Aplicacion

--- src/main/java/com/codigoton/dinner/DinnerApplication.java
+++ src/main/java/com/codigoton/dinner/DinnerApplication.java
@@ -1,5 +1,7 @@
 package com.codigoton.dinner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DinnerApplication implements CommandLineRunner {
 
+	private static Logger LOG = LoggerFactory.getLogger(DinnerApplication.class);
+
 	public static void main(String[] args) {
+		LOG.info("STARTING THE APPLICATION");
 		SpringApplication.run(DinnerApplication.class, args);
+		LOG.info("APPLICATION FINISHED");
 	}
 
 	public void run(String... args) {
-		System.out.println("HOLA MUNDO");
+		LOG.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}", i, args[i]);
+		}
 	}
 }
