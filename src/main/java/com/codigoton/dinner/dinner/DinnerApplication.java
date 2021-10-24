77e6af3bd9dd95ee2ba89a9bc790e533cc96d071
Angel Sanchez <ajsanchez22@gmail.com>
2021-10-24 02:08:46 -0500

    Project Base

---
+++ src/main/java/com/codigoton/dinner/dinner/DinnerApplication.java
@@ -0,0 +1,17 @@
+package com.codigoton.dinner.dinner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerApplication.class, args);
+	}
+
+	public void run(String... args) {
+		System.out.println("HOLA MUNDO");
+	}
+}
---
+++ src/test/java/com/codigoton/dinner/dinner/DinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigoton.dinner.dinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
